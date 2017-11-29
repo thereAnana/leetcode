8f8fb00d5d12e8420c9a65df593102acf5caa181
walnutbear <525515270@qq.com>
2017-11-29 20:05:34 +0800

    Num.419
    2D-Array operation

--- 419_Battleships_in_a_Board.js
+++ 419_Battleships_in_a_Board.js
@@ -3,7 +3,13 @@
  * @return {number}
  */
 var countBattleships = function(board) {
-     
-Add to List
-419. Battleships in a Board
+     if(board.length ==0) return 0;
+     var count = 0;
+     for(var i = 0;i<board.length;++i){
+         for(var i = 0;j<board[0].length;++j){
+             if(board[i][j] == '.'|| (i>0&&(board[i-1][j] == 'X') )||(j > 0 && board[i][j-1] == 'X')) continue;
+             count++;
+         }
+     }
+     return count;
 };
