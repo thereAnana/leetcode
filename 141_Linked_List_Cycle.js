44ad7fd0c310fb6d9723a806692dd155a55ecae6
walnutbear <525515270@qq.com>
2017-12-05 10:02:48 +0800

    Num.141
    listnode travel

---
+++ 141_Linked_List_Cycle.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(head==null) return false;
+    var walker = head;
+    var runner = head;
+    while(runner.next!=null && runner.next.next!=null) {
+        walker = walker.next;
+        runner = runner.next.next;
+        if(walker==runner) return true;
+    }
+    return false;
+};
