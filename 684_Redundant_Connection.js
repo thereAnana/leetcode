be13f3e7edadbcab7441a840fb1a57cb1b706bff
walnutbear <525515270@qq.com>
2017-12-20 15:32:16 +0800

    Num.684
    union find

--- 684_Redundant_Connection.js
+++ 684_Redundant_Connection.js
@@ -1,7 +1,18 @@
-/**
- * @param {number[][]} edges
- * @return {number[]}
- */
+
+
 var findRedundantConnection = function(edges) {
-    
+    var parent = [];
+    for(var i =0 ;i<2001;++i) parent.push(i);
+    for(var j =0 ;j<edges.length;++j){
+        var f = edges[j][0], t = edges[j][1];
+        if(find(parent,f) == find(parent,t)) return edges[j];
+        else parent[find(parent,f)] = find(parent, t);
+    }
+    return [];
 };
+var find = function(parent, f){
+    if(f != parent[f]){
+        parent[f] = find(parent,parent[f]);
+    }
+    return parent[f];
+}
