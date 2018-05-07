7798762c237df5ae9b89fa3ad1e9325cd22daa88
lina <525515270@qq.com>
2018-05-07 14:16:51 +0800

    Num.230
    binary Search

---
+++ 230_Kth_Smallest_Element_in_a_BST.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    return convertToArray(root)[k - 1];
+};
+function convertToArray(root){
+    if(root == null) return [];
+    else {
+        var list = convertToArray(root.left);
+        lisr.push(root.val);
+        Array.prototype.push.apply(list, convertToArray(root.right));
+        return list;
+    }
+}
